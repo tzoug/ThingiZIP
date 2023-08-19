4386f7e867e28d73ca616b5f520d618387820d91
tzoug <dylan.apardian@gmail.com>
2023-08-18 20:51:15 -0400

    Created Version component

--- thingizip/src/components/version.svelte
+++ thingizip/src/components/version.svelte
@@ -1,1 +1,10 @@
-<span class="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">v3.0</span>
+<script lang="ts">    
+    export let currentVersion: string;
+    currentVersion = chrome.runtime.getManifest().version;
+</script>
+
+<a href="https://github.com/tzoug/ThingiZIP/releases" target="_blank" class="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-sky-600 dark:text-stone-50">
+    v{currentVersion}
+</a>
+
+

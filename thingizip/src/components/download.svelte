bd415751bc2209fec6d6f73f61e07a33dc7ac6a9
tzoug <dylan.apardian@gmail.com>
2023-08-26 15:05:41 -0400

    Renamed button

--- thingizip/src/components/download.svelte
+++ thingizip/src/components/download.svelte
@@ -89,6 +89,6 @@
     type="button"
     on:click={() => downloadFull()}
     class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-blue-800 shadow-lg shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-    Download Zip
+    Download All
   </button>
 {/if}
